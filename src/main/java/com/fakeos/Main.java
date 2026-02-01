c3cde0bceaaaa416d078fd74a60abc4b38fb29a1
joaopedrosantanamotalol <jp1119174@gmail.com>
2026-02-01 13:54:29 -0300

    adcionada  base da telaTerminal e chamada de telaTerminal

--- src/main/java/com/fakeos/Main.java
+++ src/main/java/com/fakeos/Main.java
@@ -1,7 +1,15 @@
 package com.fakeos;
 
+import javax.swing.SwingUtilities;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // forma segura de criar e editar a interface gráfica
+        SwingUtilities.invokeLater(() -> {
+            // new terminal é a instancia do construtor da pagina
+            // setVisible(true) diz que a tela É visivel kkkkkkk
+            new TelaTerminal().setVisible(true);
+        });
+
     }
 }
---
+++ src/main/java/com/fakeos/TelaTerminal.java
@@ -0,0 +1,27 @@
+package com.fakeos;
+
+import java.awt.Color;
+//comentado para nao ficar com warning chato import javax.swing.JFileChooser;
+import javax.swing.*;
+
+public class TelaTerminal extends JFrame {
+    JPanel painel = new JPanel(); // instancia do painel
+    JLabel titulo = new JLabel("BEM-VINDO"); // instancia do titulo presente
+
+    public TelaTerminal() {
+
+        setTitle("Tela Terminal"); // define titulo da barra do app
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // você sai do programa quando fecha
+        setSize(600, 600); // declara o tamanho da tela
+        setLocationRelativeTo(null); // o posicionamento ta relativo a nada
+        painel.setLayout(null); // com o setLauout null você pode definir as posições dos objetos
+        painel.setBackground(Color.BLACK); // define a cor do painel
+
+        titulo.setForeground(Color.GREEN); // declara o objeto titulo
+        titulo.setBounds(280, 50, 200, 20); // posiciona o titulo dentro do painel x,y,width,height
+
+        add(painel); // adciona o painel a tela
+        painel.add(titulo); // adciona o titulo ao painel
+    }
+
+}
