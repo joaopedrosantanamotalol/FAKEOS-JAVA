d8cf8598d8ef6884a6dfe973f02c4542198f37f1
joaopedrosantanamotalol <jp1119174@gmail.com>
2026-02-02 14:58:00 -0300

    update(TelaTerminal): Comentários Adcionados

--- src/main/java/com/fakeos/telas/TelaTerminal.java
+++ src/main/java/com/fakeos/telas/TelaTerminal.java
@@ -16,49 +16,49 @@ public class TelaTerminal extends JFrame {
     public TelaTerminal() {
 
         // ===== JFRAME =====
-        setTitle("Tela Terminal");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(600, 600);
-        setLocationRelativeTo(null);
-        setLayout(new BorderLayout());
+        setTitle("Tela Terminal"); // define titulo
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //fala que é pra sair quando fechar 
+        setSize(600, 600); // tamanho da janela
+        setLocationRelativeTo(null); // relativo a nada
+        setLayout(new BorderLayout()); // layout definido como BorderLayout
 
         // ===== PAINEL TOPO (ALTURA FIXA) =====
-        painelTopo.setPreferredSize(new Dimension(600, 50));
-        painelTopo.setBackground(Color.BLACK);
-        painelTopo.setLayout(new BoxLayout(painelTopo, BoxLayout.Y_AXIS));
+        painelTopo.setPreferredSize(new Dimension(600, 50)); // panel width 600 height 50
+        painelTopo.setBackground(Color.BLACK); // define a cor como preta
+        painelTopo.setLayout(new BoxLayout(painelTopo, BoxLayout.Y_AXIS)); // layout baseado no eixo Y
 
         // ===== PAINEL BAIXO (OCUPA O RESTO) =====
-        painelBaixo.setBackground(Color.BLACK);
-        painelBaixo.setLayout(new BoxLayout(painelBaixo, BoxLayout.Y_AXIS));
+        painelBaixo.setBackground(Color.BLACK); // define a cor como preto
+        painelBaixo.setLayout(new BoxLayout(painelBaixo, BoxLayout.Y_AXIS));// layout baseado no eixo Y
 
-        titulo.setForeground(Color.GREEN);
-        titulo.setAlignmentX(Component.CENTER_ALIGNMENT);
+        titulo.setForeground(Color.GREEN); // define a cor como verde
+        titulo.setAlignmentX(Component.CENTER_ALIGNMENT); // alinha ao centro
 
-        painelTopo.add(Box.createVerticalGlue());
-        painelTopo.add(titulo);
-        painelTopo.add(Box.createVerticalGlue());
+        painelTopo.add(Box.createVerticalGlue());// componente invisivel que age de forma elastica
+        painelTopo.add(titulo);// adciona titulo ao painel de cima
+        painelTopo.add(Box.createVerticalGlue());// componente invisivel que age de forma elastica
 
-        introducao.setVisible(false);
-        introducao.setForeground(Color.GREEN);
+        introducao.setVisible(false); // define introducao como invisivel
+        introducao.setForeground(Color.GREEN); // define a cor verde (mesmo que esteja invisivel)
 
-        Timer timerIntro = new Timer(1500, e -> introducao.setVisible(true));
-        Timer timerComandos = new Timer(2500, e -> {
+        Timer timerIntro = new Timer(1500, e -> introducao.setVisible(true)); //timer para fazer introducao ser visivel
+        Timer timerComandos = new Timer(2500, e -> { // timer para criar as Jlabel's baseada em labels
             for (JLabel label : labels) {
-                label.setVisible(true);
+                label.setVisible(true);// define como visivel
             }
         });
 
-        timerIntro.setRepeats(false);
-        timerComandos.setRepeats(false);
+        timerIntro.setRepeats(false);// fala pro timer nao repetir
+        timerComandos.setRepeats(false);// fala pro timer nao repetir
 
-        timerIntro.start();
-        timerComandos.start();
+        timerIntro.start();// começa a ação do timer
+        timerComandos.start();// começa a ação do timer
 
-        painelBaixo.add(introducao);
+        painelBaixo.add(introducao);// adciona introducao no painelBaixo
 
-        painelBaixo.add(Box.createVerticalStrut(10));
+        painelBaixo.add(Box.createVerticalStrut(10));// adciona espaço invisivel com o tamanho de 10 px
         
-        for (int i = 0; i < comandos.length; i++) {
+        for (int i = 0; i < comandos.length; i++) {// criação de cada JLabel com o laço for
             labels[i] = new JLabel(comandos[i]);
             labels[i].setForeground(Color.GREEN);
             labels[i].setVisible(false);
