71ec7d9cabc552891c9865fb5b93223e9458ca85
lipedeoliveira <felipe.vieira.7721@gmail.com>
2026-02-03 19:36:55 +0000

    update(criarArquivos): Refatoração do código para facilidade de lógica

--- src/main/java/com/fakeos/comandos/criarPasta.java
+++ src/main/java/com/fakeos/comandos/criarPasta.java
@@ -2,4 +2,22 @@ package com.fakeos.comandos;
 import java.nio.file.*;
 public class criarPasta {
     
+    /**
+    public void createDirectory(String nome){
+        Path dirPath = Paths.get(nome);
+        try{
+            if(){
+            
+            }else{
+                Files.createDirectory(dirPath);
+                }
+        }catch(IOException e){
+            System.out.println(e);
+        }
+    }
+*/
+
+    public void checkDirectoryExistence(){
+
+    }
 }
